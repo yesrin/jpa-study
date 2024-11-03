c9bddb05bd823a7724f0c38dac69340fdcc753e9
hobit22 <hobit22@naver.com>
2024-11-03 21:36:38 +0900

    [ADD] init setting

---
+++ src/test/java/mataedu/jpastudy/JpaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package mataedu.jpastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
